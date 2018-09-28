b158ce218d17eff33c1095c693e8bb3910a1bb84
Fiorella Atoche <fa98@cornell.edu>
2018-09-28 04:33:35 -0400

    css and header added

---
+++ includes/init.php
@@ -0,0 +1,7 @@
+<header id="head">
+
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+
+  <h1>Fiorella Atoche</h1>
+  
+</header>
--- index.php
+++ index.php
@@ -1,8 +1,22 @@
+<?php include('includes/init.php'); ?>
+
 <!DOCTYPE html>
 <html>
 
   <head>
     <h1>Welcome to my website.</h1>
-    My name is Fiorella
   </head>
+
+  <body>
+    <div id="bio">
+      <p1>I am a student at Cornell University studying Information Science</p1>
+    </div>
+
+    <div id="work">
+      <p1>There are some of the things I have worked on.</p1>
+    </div>
+
+
+  </body>
+
 </html>
